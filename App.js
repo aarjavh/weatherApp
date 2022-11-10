071e4a8949824710a9dd34ab94e23ec1befb29d1
Aarjav Hansoti <aarjav.hansoti01@gmail.com>
2022-11-11 00:05:27 +0530

    first ever app

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import SearchBar from './components/SearchBar';
+import Weather from './components/Weather';
+
+const API_KEY = "3ed1e32cf1127f379b54fe8965dfd4e3";
 
 export default function App() {
+  
+  const [weatherData, setWeatherData] = useState(null);
+  const [loaded, setLoaded] = useState(true);
+  
+  async function fetchWeatherData(cityName) {
+    setLoaded(false);
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+    try {
+      const response = await fetch(API);
+      if(response.status == 200) {
+        const data = await response.json();
+        setWeatherData(data);
+      } 
+      else {
+        setWeatherData(null);
+      }
+      setLoaded(true);
+
+    } 
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchWeatherData('mumbai');
+    console.log(weatherData);
+  }, []) 
+
+  if(!loaded) {
+    return(
+      <View style={styles.container}>
+        <ActivityIndicator color='grey' size={36} />
+      </View>
+    )
+  }
+
+  else if(weatherData === null) {
+    return (
+      <View style={styles.container}>
+        <SearchBar fetchWeatherData={fetchWeatherData}/>
+        <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData} />
     </View>
   );
 }
@@ -17,4 +67,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  primaryText: {
+    margin: 20,
+    fontSize: 28
+  }
 });
